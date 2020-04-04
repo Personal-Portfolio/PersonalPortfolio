2ed80788844cdbfa0227fe09e5ec462203121b18
Evgeny Popodyanets <pop4ikxp@mail.ru>
2020-04-05 01:35:13 +0300

    cache security types in a state

--- PersonalPortfolio.Frontends/projects/personal-portfolio/src/app/features/administration/state/security-types/security-types.effects.ts
+++ PersonalPortfolio.Frontends/projects/personal-portfolio/src/app/features/administration/state/security-types/security-types.effects.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap, catchError, withLatestFrom } from 'rxjs/operators';
 
 import { State } from '../administration.state';
 import { actionSecurityTypesRequestAll, actionSecurityTypesFailRequest, actionSecurityTypesUpsertMany } from './security-types.actions';
 import { SecurityTypesService } from '../../services/securities-types.service';
 import { of } from 'rxjs';
+import { selectAllSecurityTypes } from './security-types.selectors';
 
 @Injectable()
 export class SecurityTypesEffects {
@@ -20,15 +21,23 @@ export class SecurityTypesEffects {
     () => this.actions$
             .pipe(
                 ofType(actionSecurityTypesRequestAll),
-                switchMap(() => this.securityTypesService.getAll()
+                withLatestFrom(
+                    this.store.select(selectAllSecurityTypes),    
+                    (action, store) => store
+                ),
+                switchMap((list) => {
+                    if(list && list.length > 0) {
+                        return [actionSecurityTypesUpsertMany({ securityTypes: list })];
+                    }
+
+                    return this.securityTypesService.getAll()
                     .pipe(
-                        map(items => actionSecurityTypesUpsertMany({ securityTypes: items }),
-                        catchError(error =>
-                            of(actionSecurityTypesFailRequest({ error: error }))
-                          )
+                        map(
+                            items => actionSecurityTypesUpsertMany({ securityTypes: items }),
+                            catchError(error => of(actionSecurityTypesFailRequest({ error: error })))
                         )
-                    )
-                )
+                    );
+                })
             ),
     { dispatch: true }
   );
